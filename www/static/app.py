3fc848e9254cdd9f6c4212978521860de2045506
sunyongji <363148025@qq.com>
2018-01-12 19:28:17 +0800

    更改了app

--- www/static/app.py
+++ www/static/app.py
@@ -5,11 +5,12 @@ from  datetime import datetime
 from aiohttp import web
 
 def index(request):
+
     return web.Response(body=b'<h1>Awesome</h1>',content_type="text/html")
 
 async def init(loop):
     app=web.Application(loop=loop)
-    app.router.add_route('GET','/', index)
+    app.router.add_route('GET','/1', index)
     srv = await loop.create_server(app.make_handler(),'127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
